16f8ac1a1c27adc7adfc87aa71277838d6800b45
Ivan Ermak <ermak.ivan.iv@gmail.com>
2018-12-07 18:15:59 +0300

    Initial Commit

---
+++ PropertyList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PropertyList
+//
+//  Created by Ivan Ermak on 12/7/18.
+//  Copyright © 2018 Ivan Ermak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
